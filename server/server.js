512a4743af15946e80c4505186a3369ac40cb0ca
Derek Mailer <derek.mailer@gmail.com>
2018-07-30 18:56:18 +0100

    Adding GET /todos/:d

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,6 +27,24 @@ app.get('/todos', (req, res) => {
 		res.send({todos})
 	}, (e) => {
 		res.status(400).send(e);
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id
+
+	// validate id or stop with 404
+	if (!ObjectID.isValid(id)) {
+		return res.sendStatus(404);
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (todo) {
+			return res.send({todo});
+		}
+		return res.sendStatus(404);
+	}, (e) => {
+		return res.sendStatus(400);
 	});
 });
 
